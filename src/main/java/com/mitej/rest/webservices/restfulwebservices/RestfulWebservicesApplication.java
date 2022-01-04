a8640e46e070947b6c7b25193bb2640339dc3c11
mitej23 <mitejmadan@gmail.com>
2022-01-04 21:02:55 +0530

    added spring initalizer project

---
+++ src/main/java/com/mitej/rest/webservices/restfulwebservices/RestfulWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.mitej.rest.webservices.restfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mitej/rest/webservices/restfulwebservices/RestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitej.rest.webservices.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
